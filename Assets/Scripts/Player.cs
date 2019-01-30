03dccad5488986dd3b9470db95c06afc08d58f6d
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-01-30 15:19:35 +0100

    Create movement boundaries (only within viewport)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float padding = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-		
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // Use this for initialization
+    void Start () {
+        SetupMoveBoundaries();
 	}
 	
 	// Update is called once per frame
@@ -20,9 +26,19 @@ public class Player : MonoBehaviour {
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x -padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
 }
