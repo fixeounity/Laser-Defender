00db9ee286d7c3e12d27a5f1de9420f1c7336164
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-01-31 15:59:22 +0100

    Get waypoints for enemy pathing from wave config

--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -13,10 +13,20 @@ public class WaveConfig : ScriptableObject {
     [SerializeField] float moveSpeed = 2f;
 
     public GameObject GetEnemyPrefab() { return enemyPrefab; }
-    public GameObject GetPathPrefab() { return pathPrefab;  }
     public float GetTimeBetweenSpawns() { return timeBetweenSpawns;  }
     public float GetSpawnRandomFactor() { return spawnRandomFactor; }
     public int GetNumberOfEnemies() { return numberOfEnemies;  }
     public float GetMoveSpeed() { return moveSpeed; }
-    
+
+    public List<Transform> GetWaypoints()
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach(Transform child in pathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
+
+
 }
