4910da9b5b3ac277cc92ef3200841ddda3e09a79
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-08 12:50:19 +0100

    Add collision layers for projectile, enemy and player + Destroy projectile on hit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -85,6 +85,7 @@ public class Player : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
@@ -94,7 +95,7 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         var damageDealer = other.GetComponent<DamageDealer>();
-
+        if (!damageDealer) return;
         ProcessHit(damageDealer);
     }
 
