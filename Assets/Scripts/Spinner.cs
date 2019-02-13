3c9609822b46621e5067807d5134fa2b951f0e4d
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-02-13 15:43:28 +0100

    Add sorting layer fand spinning rotation for projectiles

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+    [SerializeField] float spinSpeed = 90;
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+	}
+
+    private void Rotate()
+    {
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    }
+}
